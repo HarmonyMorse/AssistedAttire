bd98b9388fa42fd2f1c9dec19681c97f9652700f
HarmonyMorse <harmonythedev@gmail.com>
2024-02-15 07:39:17 -0800

    Add Wardrobe component

---
+++ src/components/Wardrobe.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom"
+
+const Wardrobe = ({ clothingItems }) => {
+    return (
+        <div>
+            <h1>Wardrobe</h1>
+            <ul>
+                {clothingItems.map((item) => (
+                    <li key={item.id}>
+                        <Link to={`/wardrobe/${item._id}`}>
+                            <h2>{item.name}</h2>
+                        </Link>
+                        <p>Notes: {item.notes}</p>
+                        <p>Occasion: {item.occasion}</p>
+                        <p>Category: {item.category}</p>
+                        <p>Weather: {item.weather}</p>
+                        <p>Colors: {item.colors}</p>
+                        <p>Brand: {item.brand}</p>
+                        <p>Size: {item.size}</p>
+                        <p>Status: {item.status}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default Wardrobe
