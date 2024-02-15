fe20937e26341f6a1b2447c956e0ca2e9d81a5a6
HarmonyMorse <harmonythedev@gmail.com>
2024-02-15 07:38:51 -0800

    Add clothingItems state and wardrobe route to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import Home from './components/Home'
+import { useEffect, useState } from 'react'
+import Client from './assets/services/api'
 import Nav from './components/Nav'
+import Home from './components/Home'
 import NewItem from './components/NewItem'
+import Wardrobe from './components/Wardrobe'
 
 const App = () => {
+  const [clothingItems, setClothingItems] = useState([])
+
+  useEffect(() => {
+    const getClothingItems = async () => {
+      let res = await Client.get('/clothingItems')
+      setClothingItems(res.data)
+    }
+    getClothingItems()
+  }, [])
+
 
   return (
     <div className="App">
@@ -14,6 +27,7 @@ const App = () => {
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/wardrobe" element={<Wardrobe clothingItems={ clothingItems } />} />
           <Route path="/wardrobe/new" element={<NewItem />} />
         </Routes>
       </main>
