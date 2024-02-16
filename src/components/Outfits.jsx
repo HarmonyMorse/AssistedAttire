a6775c9b25526e38450a7abc965e2c7ca06a81bc
HarmonyMorse <harmonythedev@gmail.com>
2024-02-16 09:14:23 -0800

    Update link styles and remove unused code

--- src/components/Outfits.jsx
+++ src/components/Outfits.jsx
@@ -4,16 +4,16 @@ const Outfits = ({ outfits }) => {
     return (
         <div>
             <h1>Outfits</h1>
-            <Link to="/outfits/new">Add Outfit</Link>
+            <Link to="/outfits/new" className="button">Add Outfit</Link>
             <ul>
                 {outfits.map((outfit) => (
                     <li key={outfit._id}>
                         <Link to={`/outfits/${outfit._id}`}>
                             <h2>{outfit.name}</h2>
                         </Link>
-                        <p>Occasion: {outfit.occasion}</p>
-                        <p>Weather: {outfit.weather}</p>
-                        <p>Notes: {outfit.notes}</p>
+                        {/* <p>Occasion: {outfit.occasion}</p> */}
+                        {/* <p>Weather: {outfit.weather}</p> */}
+                        <p>{outfit.notes}</p>
                     </li>
                 ))}
             </ul>
--- src/components/Wardrobe.jsx
+++ src/components/Wardrobe.jsx
@@ -4,20 +4,20 @@ const Wardrobe = ({ clothingItems }) => {
     return (
         <div>
             <h1>Wardrobe</h1>
-            <Link to="/wardrobe/new">Add Clothing Item</Link>
+            <Link to="/wardrobe/new" className="button">Add Clothing Item</Link>
             <ul>
                 {clothingItems.map((item) => (
                     <li key={item._id}>
                         <Link to={`/wardrobe/${item._id}`}>
                             <h2>{item.name}</h2>
                         </Link>
-                        <p>Notes: {item.notes}</p>
-                        <p>Occasion: {item.occasion}</p>
+                        {/* <p>Notes: {item.notes}</p> */}
+                        {/* <p>Occasion: {item.occasion}</p> */}
                         <p>Category: {item.category}</p>
-                        <p>Weather: {item.weather}</p>
-                        <p>Colors: {item.colors}</p>
-                        <p>Brand: {item.brand}</p>
-                        <p>Size: {item.size}</p>
+                        {/* <p>Weather: {item.weather}</p> */}
+                        {/* <p>Colors: {item.colors}</p> */}
+                        {/* <p>Brand: {item.brand}</p> */}
+                        {/* <p>Size: {item.size}</p> */}
                         <p>Status: {item.status}</p>
                     </li>
                 ))}
