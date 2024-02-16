b3ddb009799de031c98a6f79dd1f0ada9046cae4
HarmonyMorse <harmonythedev@gmail.com>
2024-02-16 09:28:11 -0800

    Add backend link

--- src/assets/services/api.js
+++ src/assets/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const BASE_URL = ''
+export const BASE_URL = 'https://assisted-attire-server-cf6d32af0aa5.herokuapp.com'
 
 const Client = axios.create({baseURL: BASE_URL})
 
