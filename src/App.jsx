ec5d62cb9a503ddfff7733c891f63923b83e501d
HarmonyMorse <harmonythedev@gmail.com>
2024-02-16 09:15:16 -0800

    Add wishlist routes to App

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,15 @@ import Outfits from './components/Outfits'
 import NewOutfit from './components/NewOutfit'
 import OutfitDetail from './components/OutfitDetail'
 import OutfitEdit from './components/OutfitEdit'
+import Wishlist from './components/Wishlist'
+import NewWishlistItem from './components/NewWishlistItem'
+import WishlistItemDetail from './components/WishlistItemDetail'
+import WishlistItemEdit from './components/WishlistItemEdit'
 
 const App = () => {
   const [clothingItems, setClothingItems] = useState([])
   const [outfits, setOutfits] = useState([])
+  const [wishlist, setWishlist] = useState([])
 
   const getClothingItems = async () => {
     let res = await Client.get('/clothingItems')
@@ -26,9 +31,15 @@ const App = () => {
     setOutfits(res.data)
   }
 
+  const getWishlist = async () => {
+    let res = await Client.get('/wishlist')
+    setWishlist(res.data)
+  }
+
   useEffect(() => {
     getClothingItems()
     getOutfits()
+    getWishlist()
   }, [])
 
 
@@ -47,6 +58,10 @@ const App = () => {
           <Route path="/outfits/new" element={<NewOutfit clothingItems={clothingItems} />} />
           <Route path="/outfits/:id" element={<OutfitDetail />} />
           <Route path="/outfits/:id/edit" element={<OutfitEdit clothingItems={clothingItems} />} />
+          <Route path="/wishlist" element={<Wishlist wishlist={wishlist} />} />
+          <Route path="/wishlist/new" element={<NewWishlistItem />} />
+          <Route path="/wishlist/:id" element={<WishlistItemDetail />} />
+          <Route path="/wishlist/:id/edit" element={<WishlistItemEdit />} />
         </Routes>
       </main>
     </div>
